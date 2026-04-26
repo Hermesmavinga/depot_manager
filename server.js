919006b9ea4bedfd02b06fe9f15ef98ceaa1f4fb
Hermes Mavinga <hermesmavinga2019@gmail.com>
2026-04-26 22:09:36 +0100

    update server js

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const jsonServer = require("json-server");
+const path = require("path");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
+
+// CORRECTION : On indique le chemin réel vers le dossier database
+const router = jsonServer.router(path.join(__dirname, "database", "db.json"));
+
+// CORRECTION : On active le service des fichiers statiques (ton HTML/JS)
+const middlewares = jsonServer.defaults({
+  static: "./",
+});
+
 const port = process.env.PORT || 10000;
 
 server.use(middlewares);
 server.use(router);
 
 server.listen(port, () => {
-  console.log("JSON Server est lancé sur le port ${port}");
+  console.log(`JSON Server est lancé sur le port ${port}`);
 });
